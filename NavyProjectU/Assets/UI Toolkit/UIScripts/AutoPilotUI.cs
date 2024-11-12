e4d27f538b4fc0da1cfb6e173766c775fd1dc7eb
zzzack77 <125263916+zzzack77@users.noreply.github.com>
2024-11-12 15:29:02 +0000

    Text altering and more names.

--- NavyProjectU/Assets/UI Toolkit/UIScripts/AutoPilotUI.cs
+++ NavyProjectU/Assets/UI Toolkit/UIScripts/AutoPilotUI.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+//using System.Reflection.Emit;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,43 +9,75 @@ public class AutoPilotUI : MonoBehaviour
 {
     private UIDocument _document;
 
+    //LHS buttons
     private Button remoteButton;
     private Button navButton;
     private Button autoButton;
     private Button manualButton;
     private Button nfuButton;
 
-    
+    //Text
+    private Label _headingOutputL;
+
+
+
     private void Awake()
     {
         _document = GetComponent<UIDocument>();
 
         //Query searches each button name
-        remoteButton = _document.rootVisualElement.Q("MANUALB") as Button;
-        navButton = _document.rootVisualElement.Q("NAVB") as Button;
-        autoButton = _document.rootVisualElement.Q("AUTOB") as Button;
-        manualButton = _document.rootVisualElement.Q("MANUALB") as Button;
-        nfuButton = _document.rootVisualElement.Q("NFUB") as Button;
+        remoteButton = _document.rootVisualElement.Q("remoteB") as Button;
+        navButton = _document.rootVisualElement.Q("navB") as Button;
+        autoButton = _document.rootVisualElement.Q("autoB") as Button;
+        manualButton = _document.rootVisualElement.Q("manualB") as Button;
+        nfuButton = _document.rootVisualElement.Q("nfuB") as Button;
 
 
         //example code for button press, will call function "OnAutoPress"
-        autoButton.RegisterCallback<ClickEvent>(OnAutoPress);
         
+        
+        remoteButton.RegisterCallback<ClickEvent>(OnREMOTEPress);
+        navButton.RegisterCallback<ClickEvent>(OnNAVPress);
+        autoButton.RegisterCallback<ClickEvent>(OnAUTOPress);
+        manualButton.RegisterCallback<ClickEvent>(OnMANUALPress);
+        nfuButton.RegisterCallback<ClickEvent>(OnNFUPress);
+
+
+        //explample changing text code
+        var _headingOutputL = _document.rootVisualElement.Q("headingOutputL") as Label;
+
+        _headingOutputL.text = "000.0";
 
     }
     //Unregisters each button when the UI is disableda
     private void OnDisable()
     {
-        remoteButton.UnregisterCallback<ClickEvent>(OnAutoPress);
-        navButton.UnregisterCallback<ClickEvent>(OnAutoPress);
-        autoButton.UnregisterCallback<ClickEvent>(OnAutoPress);
-        manualButton.UnregisterCallback<ClickEvent>(OnAutoPress);
-        nfuButton.UnregisterCallback<ClickEvent>(OnAutoPress);
+        remoteButton.UnregisterCallback<ClickEvent>(OnREMOTEPress);
+        navButton.UnregisterCallback<ClickEvent>(OnNAVPress);
+        autoButton.UnregisterCallback<ClickEvent>(OnAUTOPress);
+        manualButton.UnregisterCallback<ClickEvent>(OnMANUALPress);
+        nfuButton.UnregisterCallback<ClickEvent>(OnNFUPress);
     }
 
     
-    private void OnAutoPress(ClickEvent evt)
+    private void OnREMOTEPress(ClickEvent evt)
     {
-        Debug.Log("You pressed the AUTO button.");
+        UnityEngine.Debug.Log("You pressed the REMOTE button.");
+    }
+    private void OnNAVPress(ClickEvent evt)
+    {
+        UnityEngine.Debug.Log("You pressed the NAV button.");
+    }
+    private void OnAUTOPress(ClickEvent evt)
+    {
+        UnityEngine.Debug.Log("You pressed the AUTO button.");
+    }
+    private void OnMANUALPress(ClickEvent evt)
+    {
+        UnityEngine.Debug.Log("You pressed the MANUAL button.");
+    }
+    private void OnNFUPress(ClickEvent evt)
+    {
+        UnityEngine.Debug.Log("You pressed the NFU button.");
     }
 }
