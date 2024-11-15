f685c7724af1b415c5370d5d2cc5df4b857dca9b
zzzack77 <125263916+zzzack77@users.noreply.github.com>
2024-11-15 17:59:09 +0000

    buttons

--- NavyProjectU/Assets/UI Toolkit/UIScripts/AutoPilotUI.cs
+++ NavyProjectU/Assets/UI Toolkit/UIScripts/AutoPilotUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -19,6 +20,10 @@ public class AutoPilotUI : MonoBehaviour
 
     //Text
     private Label _headingOutputL;
+
+    public int a = 0;
+
+    //colours
 
 
 
@@ -47,13 +52,14 @@ public class AutoPilotUI : MonoBehaviour
 
         //explample changing text code
         
-
+        
     }
     private void Update()
     {
         var _headingOutputL = _document.rootVisualElement.Q("headingOutputL") as Label;
 
         _headingOutputL.text = _variables.Heading.ToString();
+        
         
 
     }
@@ -70,25 +76,128 @@ public class AutoPilotUI : MonoBehaviour
     
     private void OnREMOTEPress(ClickEvent evt)
     {
+        HighlightSelectedButton(0);
         UnityEngine.Debug.Log("You pressed the REMOTE button.");
         _variables.Heading = _variables.Heading + 10;
     }
     private void OnNAVPress(ClickEvent evt)
     {
+        HighlightSelectedButton(1);
+
         UnityEngine.Debug.Log("You pressed the NAV button.");
     }
     private void OnAUTOPress(ClickEvent evt)
     {
+        HighlightSelectedButton(2);
+
         _variables.IsAuto = true;
         UnityEngine.Debug.Log("You pressed the AUTO button.");
     }
     private void OnMANUALPress(ClickEvent evt)
     {
+        HighlightSelectedButton(3);
+
+        manualButton.AddToClassList("buttonClicked");
+
         _variables.IsAuto = false;
         UnityEngine.Debug.Log("You pressed the MANUAL button.");
+        
     }
     private void OnNFUPress(ClickEvent evt)
     {
+        HighlightSelectedButton(4);
+
+        
         UnityEngine.Debug.Log("You pressed the NFU button.");
+    }
+    private void HighlightSelectedButton(int number)
+    {
+        //variables
+        Color colorDB = Color.black;
+        Color colorGrey = Color.gray;
+        Color colorBlue = Color.blue;
+
+        
+        //hex codes for each colour
+        string hexColorDB = "#1B1D24";
+        string hexColorG = "#898989";
+        string hexColorB = "#9BEFFF";
+
+        //attempts to parse the hex colour to a unity colour
+        if (ColorUtility.TryParseHtmlString(hexColorDB, out colorDB)) ;
+        else UnityEngine.Debug.Log("error1");
+
+        if (ColorUtility.TryParseHtmlString(hexColorG, out colorGrey)) ;
+        else UnityEngine.Debug.Log("error2");
+
+        if (ColorUtility.TryParseHtmlString(hexColorB, out colorBlue)) ;
+        else UnityEngine.Debug.Log("error3");
+
+        //depending on which button is pressed, e.g. remote button = 0, nav button = 1
+        //change the background to dark blue and the text to blue and change all the other
+        //buttons back to original colour.
+        switch (number)
+        {
+            case 0:
+                remoteButton.style.backgroundColor = new StyleColor(colorDB);
+                remoteButton.style.color = new StyleColor(colorBlue);
+                navButton.style.backgroundColor = new StyleColor(colorGrey);
+                navButton.style.color = new StyleColor(Color.white);
+                autoButton.style.backgroundColor = new StyleColor(colorGrey);
+                autoButton.style.color = new StyleColor(Color.white);
+                manualButton.style.backgroundColor = new StyleColor(colorGrey);
+                manualButton.style.color = new StyleColor(Color.white);
+                nfuButton.style.backgroundColor = new StyleColor(colorGrey);
+                nfuButton.style.color = new StyleColor(Color.white);
+                break;
+            case 1:
+                remoteButton.style.backgroundColor = new StyleColor(colorGrey);
+                remoteButton.style.color = new StyleColor(Color.white);
+                navButton.style.backgroundColor = new StyleColor(colorDB);
+                navButton.style.color = new StyleColor(colorBlue);
+                autoButton.style.backgroundColor = new StyleColor(colorGrey);
+                autoButton.style.color = new StyleColor(Color.white);
+                manualButton.style.backgroundColor = new StyleColor(colorGrey);
+                manualButton.style.color = new StyleColor(Color.white);
+                nfuButton.style.backgroundColor = new StyleColor(colorGrey);
+                nfuButton.style.color = new StyleColor(Color.white);
+                break;
+            case 2:
+                remoteButton.style.backgroundColor = new StyleColor(colorGrey);
+                remoteButton.style.color = new StyleColor(Color.white);
+                navButton.style.backgroundColor = new StyleColor(colorGrey);
+                navButton.style.color = new StyleColor(Color.white);
+                autoButton.style.backgroundColor = new StyleColor(colorDB);
+                autoButton.style.color = new StyleColor(colorBlue);
+                manualButton.style.backgroundColor = new StyleColor(colorGrey);
+                manualButton.style.color = new StyleColor(Color.white);
+                nfuButton.style.backgroundColor = new StyleColor(colorGrey);
+                nfuButton.style.color = new StyleColor(Color.white);
+                break;
+            case 3:
+                remoteButton.style.backgroundColor = new StyleColor(colorGrey);
+                remoteButton.style.color = new StyleColor(Color.white);
+                navButton.style.backgroundColor = new StyleColor(colorGrey);
+                navButton.style.color = new StyleColor(Color.white);
+                autoButton.style.backgroundColor = new StyleColor(colorGrey);
+                autoButton.style.color = new StyleColor(Color.white);
+                manualButton.style.backgroundColor = new StyleColor(colorDB);
+                manualButton.style.color = new StyleColor(colorBlue);
+                nfuButton.style.backgroundColor = new StyleColor(colorGrey);
+                nfuButton.style.color = new StyleColor(Color.white);
+                break;
+            case 4:
+                remoteButton.style.backgroundColor = new StyleColor(colorGrey);
+                remoteButton.style.color = new StyleColor(Color.white);
+                navButton.style.backgroundColor = new StyleColor(colorGrey);
+                navButton.style.color = new StyleColor(Color.white);
+                autoButton.style.backgroundColor = new StyleColor(colorGrey);
+                autoButton.style.color = new StyleColor(Color.white);
+                manualButton.style.backgroundColor = new StyleColor(colorGrey);
+                manualButton.style.color = new StyleColor(Color.white);
+                nfuButton.style.backgroundColor = new StyleColor(colorDB);
+                nfuButton.style.color = new StyleColor(colorBlue);
+                break;
+        }
     }
 }
