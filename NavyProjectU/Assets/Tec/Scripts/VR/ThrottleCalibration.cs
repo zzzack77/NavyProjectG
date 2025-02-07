ac9db8ca299be8ebdf76f345f75e674191500092
SilverXOmega <bwlego@outlook.com>
2025-02-07 19:35:05 +0000

    throttle calibration script

---
+++ NavyProjectU/Assets/Tec/Scripts/VR/ThrottleCalibration.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrottleCalibration : MonoBehaviour
+{
+    public GameObject throttle;
+    public GameObject rightHand;
+    private float throttleY = 2.209249f;
+    private Vector3 offset = new Vector3(-0.00172f, 0, 0.00298f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 finalThrottlePos = new Vector3(rightHand.transform.localPosition.x, throttleY, rightHand.transform.localPosition.z);
+        if (Input.anyKeyDown)
+        {
+            throttle.transform.position = finalThrottlePos /*- offset*/;
+            throttle.transform.localRotation = Quaternion.Euler(0, rightHand.transform.eulerAngles.y, 0);
+        }
+    }
+}
