86f3d8f9305a6473d9c3075daf158e801f12a868
tjaec <87785559+tjaec@users.noreply.github.com>
2025-02-12 13:19:42 +0000

    Physics Changes
    Prop RPMs
    Better Longitudinal Drag
    Switched to FixedUpdate

--- NavyProjectU/Assets/Tec/Scripts/Physics/ShipMovement.cs
+++ NavyProjectU/Assets/Tec/Scripts/Physics/ShipMovement.cs
@@ -14,15 +14,23 @@ public class ShipMovement : MonoBehaviour
 
     public float accelInput = 0.0f;
     public float steeringInput = 0.0f;
-    public float shipPower = 200.0f;
+    public float shipPower = 3500000.0f;
 
-    public float suspensionRestDist = 5;
-    public float springStrength = 400;
-    public float springDamper = 40;
+    public float suspensionRestDist = 5.0f;
+    public float springStrength = 15000000.0f;
+    public float springDamper = 40.0f;
 
     public float dragFactor = 0.5f;
-    public float boatWeight = 1;
-    public float boatTopSpeed = 1;
+    public float dragCoefficient = 425000.0f;
+    public float boatWeight = 1.0f;
+
+    private float rpmEngineMax = 3000.0f;
+    private float rpmEngineStarboard = 0.0f;
+    private float rpmEnginePort = 0.0f;
+
+    public float rpmPropMax = 250.0f;
+    public float rpmPropStarboard = 0.0f;
+    public float rpmPropPort = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -31,13 +39,15 @@ public class ShipMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {       
 
         Vector3 forwardDirection = transform.forward;
 
-        float boatSpeedkph = rb.velocity.magnitude * 3.6f;
-        float boatSpeedkn = rb.velocity.magnitude * 1.944f;
+        var LocalV = transform.InverseTransformDirection(rb.velocity);
+
+        float boatSpeedkph = LocalV.z * 3.6f;
+        float boatSpeedkn = LocalV.z * 1.944f;
 
         UnityEngine.Debug.Log("Velocity: " + boatSpeedkph + "km/h");
         UnityEngine.Debug.Log("Velocity: " + boatSpeedkn + "knots");
@@ -49,7 +59,7 @@ public class ShipMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             
-            accelInput = accelInput + 0.001f;
+            accelInput = accelInput + 0.01f;
             
             if(accelInput > 1.0f)
             {
@@ -58,7 +68,7 @@ public class ShipMovement : MonoBehaviour
         }
         else if(Input.GetKey(KeyCode.S))
         {
-            accelInput = accelInput - 0.001f;
+            accelInput = accelInput - 0.01f;
 
             if (accelInput < -1.0f)
             {
@@ -66,12 +76,62 @@ public class ShipMovement : MonoBehaviour
             }
         }
 
+
+        // RPM code
+
+        float rpmInput = accelInput * rpmPropMax;
+
+        // Starborad Propeller
+
+        if (rpmInput > rpmPropStarboard)
+        {
+            rpmPropStarboard = rpmPropStarboard + 1.0f;
+            if(rpmPropStarboard > rpmInput)
+            {
+                rpmPropStarboard = rpmInput;
+            }
+
+        }
+        else if(rpmInput < rpmPropStarboard)
+        {
+            rpmPropStarboard = rpmPropStarboard - 1.0f;
+            if (rpmPropStarboard < rpmInput)
+            {
+                rpmPropStarboard = rpmInput;
+            }
+        }
+
+        // Port Propeller
+
+        if (rpmInput > rpmPropPort)
+        {
+            rpmPropPort = rpmPropPort + 1.0f;
+            if (rpmPropPort > rpmInput)
+            {
+                rpmPropPort = rpmInput;
+            }
+
+        }
+        else if (rpmInput < rpmPropPort)
+        {
+            rpmPropPort = rpmPropPort - 1.0f;
+            if (rpmPropPort < rpmInput)
+            {
+                rpmPropPort = rpmInput;
+            }
+        }
+
+        //UnityEngine.Debug.Log("Input RPM: " + rpmInput);
+        //UnityEngine.Debug.Log("Starboard Prop RPM: " + rpmPropStarboard);
+        //UnityEngine.Debug.Log("Port Prop RPM: " + rpmPropPort);
+
+
         // Steering code turns the rudder when the A and D keys are pressed.
         // Once the steering wheel is connected to the project, this will need to be mapped to its axis.
 
         if (Input.GetKey(KeyCode.A))
         {
-            steeringInput = steeringInput + 0.1f;
+            steeringInput = steeringInput + 0.5f;
 
             if(steeringInput > 35.0f)
             {
@@ -81,7 +141,7 @@ public class ShipMovement : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            steeringInput = steeringInput - 0.1f;
+            steeringInput = steeringInput - 0.5f;
 
             if(steeringInput < -35.0f)
             {
