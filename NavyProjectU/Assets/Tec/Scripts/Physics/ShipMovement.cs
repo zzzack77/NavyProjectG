62465849b0664f056324703eae81e3d208f1d548
zzzack77 <125263916+zzzack77@users.noreply.github.com>
2025-02-13 17:06:17 +0000

    Code clean up with added debugging functions and rate of turn

--- NavyProjectU/Assets/Tec/Scripts/Physics/ShipMovement.cs
+++ NavyProjectU/Assets/Tec/Scripts/Physics/ShipMovement.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
+
 using System.Security.Cryptography;
 
 
@@ -27,6 +27,12 @@ public class ShipMovement : MonoBehaviour
     public float boatWeight;
     public float boatTopSpeed;
 
+    [Header("Boat stats")]
+    public float boatSpeedkph;
+    public float boatSpeedkn;
+    public float rateOfTurn;
+
+    public bool isThrottleConnected;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,96 +40,71 @@ public class ShipMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        privateVariables.Heading = transform.rotation.eulerAngles.y;
+        if (privateVariables != null) { privateVariables.Heading = transform.rotation.eulerAngles.y; }
+        UpdateInspectorvariables();
     }
 
     // Update is called once per frame
     void Update()
     {       
+        // Resets position / position, rotation and velocity back to 0 for debugging purposes
+        if (Input.GetKey(KeyCode.Z)) { ResetPos(); }
+        if (Input.GetKey(KeyCode.X)) { ResetPosRotVelocity(); }
 
-        Vector3 forwardDirection = transform.forward;
-
-        float boatSpeedkph = rb.velocity.magnitude * 3.6f;
-        float boatSpeedkn = rb.velocity.magnitude * 1.944f;
-
-        //UnityEngine.Debug.Log("Velocity: " + boatSpeedkph + "km/h");
-        //UnityEngine.Debug.Log("Velocity: " + boatSpeedkn + "knots");
-
-
-        // Throttle code slowly ups the throttle the longer you press the forward and backward keys.
-        // This will need to be mapped to the throttle axis once we connect it all up.
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            
-            accelInput = accelInput + 0.001f;
-            
-            if(accelInput > 1.0f)
-            {
-                accelInput = 1.0f;
-            }
-        }
-        else if(Input.GetKey(KeyCode.S))
-        {
-            accelInput = accelInput - 0.001f;
-
-            if (accelInput < -1.0f)
-            {
-                accelInput = -1.0f;
-            }
-        }
-
-        // Steering code turns the rudder when the A and D keys are pressed.
-        // Once the steering wheel is connected to the project, this will need to be mapped to its axis.
-
+        VerticalMovement();
+        // Depending on autopilot nfu or manual call correct function for movement
         if (privateVariables != null)
         {
-            // Auto pilot
-            if (privateVariables.IsAuto)
-            {
-                float difference = (privateVariables.SetAutoCourse - privateVariables.Heading + 360) % 360;
-                UnityEngine.Debug.Log(difference);
-                
-                if (difference <= 180)
-                {
-                    steeringInput = -1f;
-                }
-                else
-                {
-                    steeringInput = 1;
-                }
-                //UnityEngine.Debug.Log("auto");
-            }
+            if (privateVariables.IsAuto) { AutoPilotMode(); }
+            else if (privateVariables.IsNFU) { NFUMode(); }
+            else { ManualMode(); }
+        }
+        // If private variabels is null defult to manual mode (A, D key presses)
+        else { ManualMode(); }
+    }
 
-            // Manual code, insert steering wheel code here
-            if (privateVariables.IsManual)
-            {
-                if (Input.GetKey(KeyCode.A))
-                {
-                    steeringInput = steeringInput + 0.1f;
+    // Resets boat without restarting the scene
+    public void ResetPosRotVelocity()
+    {
+        transform.position = new Vector3(0, 5, 0);
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        rb.velocity = new Vector3(0, 0, 0);
+    }
 
-                    if (steeringInput > 35.0f)
-                    {
-                        steeringInput = 35.0f;
-                    }
-
-                }
-                else if (Input.GetKey(KeyCode.D))
-                {
-                    steeringInput = steeringInput - 0.1f;
-
-                    if (steeringInput < -35.0f)
-                    {
-                        steeringInput = -35.0f;
-                    }
-                }
-            }
-            // NFU code
-            if (privateVariables.IsNFU)
-            {
-                steeringInput = -privateVariables.NfuSteeringValue;
-            }
+    // Resets position, can be used to test long distant movement without boat falling of the world
+    public void ResetPos() { transform.position = new Vector3(0, 5, 0); }
+    public void UpdateInspectorvariables()
+    {
+        boatSpeedkph = rb.velocity.magnitude * 3.6f;
+        boatSpeedkn = rb.velocity.magnitude * 1.944f;
+        rateOfTurn = rb.angularVelocity.y * Mathf.Rad2Deg;
+    }
+    public void VerticalMovement()
+    {
+        if (isThrottleConnected)
+        {
+            //Put throttle code here
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.W) && accelInput < 1.0f) { accelInput = accelInput + 0.001f; }
+            else if (Input.GetKey(KeyCode.S) && accelInput > -1) { accelInput = accelInput - 0.001f; }
         }
         
     }
+    // Auto Pilot code
+    public void AutoPilotMode()
+    {
+        float difference = (privateVariables.SetAutoCourse - privateVariables.Heading + 360) % 360;
+        UnityEngine.Debug.Log(difference);
+
+        if (difference <= 180) { steeringInput = -1f; }
+        else { steeringInput = 1; }
+    }
+    public void ManualMode()
+    {
+        if (Input.GetKey(KeyCode.A) && steeringInput < 35.0f) { steeringInput = steeringInput + 0.1f; }
+        else if (Input.GetKey(KeyCode.D) && steeringInput > -35.0f) { steeringInput = steeringInput - 0.1f; }
+    }
+    public void NFUMode() { steeringInput = -privateVariables.NfuSteeringValue; }
 }
