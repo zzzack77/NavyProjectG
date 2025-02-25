169173b2c5585885f8de39b41cbee454352091e2
zzzack77 <125263916+zzzack77@users.noreply.github.com>
2025-02-25 12:03:50 +0000

    fixing tape

--- NavyProjectU/Assets/Tec/Scripts/UI/HeadingUI.cs
+++ NavyProjectU/Assets/Tec/Scripts/UI/HeadingUI.cs
@@ -8,23 +8,10 @@ public class HeadingUI : MonoBehaviour
     public RawImage HeadingImage;
     public Transform Player;
 
-    private float ye = 0.002703f;
-
-    private float headingUnit = ((14998.0f * (25.0f / 9.0f)) / 360.0f) /100f;
-  //  public Text Heading;
-
-
-    void Start()
-    {
-        Debug.Log(headingUnit);
-    }
-
-  
+    public float ye = 0.0027027f;
     void Update()
     {
-        Debug.Log(Player.localEulerAngles.y / 360);
-        HeadingImage.uvRect = new Rect((Player.localEulerAngles.y * ye) - (184.5f * ye) , 0, 1, 1);
-        //HeadingImage.uvRect = new Rect(Player.localEulerAngles.y  , 0, 1, 1);
+        HeadingImage.uvRect = new Rect((Player.localEulerAngles.y * ye) - (179.0f * ye) , 0, 1, 1);
 
         Vector3 forward = Player.transform.forward;
 
@@ -35,53 +22,5 @@ public class HeadingUI : MonoBehaviour
 
         int displayangle;
         displayangle = Mathf.RoundToInt(headingAngle);
-
-       // Heading.text = headingAngle.ToString();
-    
-
-        //switch (displayangle)
-        //{
-        //    case 0:
-        //        Heading.text = "0";
-        //        break;
-
-        //    case 360:
-        //        Heading.text = "0";
-        //        break;
-
-        //    case 45:
-        //        Heading.text = "45";
-        //        break;
-
-        //    case 90:
-        //        Heading.text = "90";
-        //        break;
-
-        //    case 130:
-        //        Heading.text = "130";
-        //        break;
-
-        //    case 180:
-        //        Heading.text = "180";
-        //        break;
-
-        //    case 225:
-        //        Heading.text = "225";
-        //        break;
-
-        //    case 270:
-        //        Heading.text = "270";
-        //        break;
-
-        //    default:
-        //        Heading.text = headingAngle.ToString();
-        //        break;
-
-
-        //}
-
-
-
-
     }
 }
