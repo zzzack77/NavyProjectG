d7be4532eca20b292279d3a9777f49c8d404debd
BenWhite2003 <bwlego@outlook.com>
2025-02-26 11:20:37 +0000

    updated heading UI

--- NavyProjectU/Assets/UI/UIScripts/Proto2.0/HeadingUI.cs
+++ NavyProjectU/Assets/UI/UIScripts/Proto2.0/HeadingUI.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HeadingUI : MonoBehaviour
 {
+    private PrivateVariables privateVariables;
+
     public RawImage HeadingImage;
-    public Transform Ship;
+    public Transform Player;
+
+    public GameObject HeadingValue;
+    public GameObject SpeedValue;
+    public GameObject ROTValue;
 
     private float ye = 1 / 360f;
     private float offset = 179.49865f;
+
+    private void Start()
+    {
+        privateVariables = GetComponent<PrivateVariables>();
+
+    }
     void Update()
     {
-        HeadingImage.uvRect = new Rect((Ship.localEulerAngles.y * ye) - offset, 0, 1, 1);
+        HeadingImage.uvRect = new Rect((Player.localEulerAngles.y * ye) - offset, 0, 1, 1);
 
-        Vector3 forward = Ship.transform.forward;
+        Vector3 forward = Player.transform.forward;
 
         forward.y = 0;
 
@@ -23,5 +36,33 @@ public class HeadingUI : MonoBehaviour
 
         int displayangle;
         displayangle = Mathf.RoundToInt(headingAngle);
+    }
+
+    public void OnHeadingUpdate()
+    {
+        TextMeshProUGUI headingText = HeadingValue.GetComponentInChildren<TextMeshProUGUI>();
+
+        if (headingText != null)
+        {
+            headingText.text = privateVariables.Heading.ToString("000.0");
+        }
+    }
+    public void OnRateOfTurnUpdate()
+    {
+        TextMeshProUGUI ROTText = ROTValue.GetComponentInChildren<TextMeshProUGUI>();
+
+        if (ROTText != null)
+        {
+            ROTText.text = privateVariables.RateOfTurn.ToString("000.0");
+        }
+    }
+    public void OnSpeedKnUpdate()
+    {
+        TextMeshProUGUI SpeedText = SpeedValue.GetComponentInChildren<TextMeshProUGUI>();
+
+        if (SpeedText != null)
+        {
+            SpeedText.text = privateVariables.SpeedKn.ToString("000.0");
+        }
     }
 }
