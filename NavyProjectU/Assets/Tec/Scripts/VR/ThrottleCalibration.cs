6563168a270ff602b5a5e86b901233d4f558b5b9
SilverXOmega <bwlego@outlook.com>
2025-03-03 01:31:05 +0000

    Throttle calibration done

--- NavyProjectU/Assets/Tec/Scripts/VR/ThrottleCalibration.cs
+++ NavyProjectU/Assets/Tec/Scripts/VR/ThrottleCalibration.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class ThrottleCalibration : MonoBehaviour
 {
-    public GameObject throttle;
+    public GameObject newThrottle;
     public GameObject rightHand;
-    public GameObject heroModelsParent;
-    private float throttleY = 2.209249f;
-    public Vector3 offset = new Vector3(0.9f, 0, 0.7f);
+    public GameObject shipGO;
+    public float throttleY = 2.209249f;
+    public float zOffset;
+    private bool throttleIsSpawned = false;
+   // public Vector3 offset = new Vector3(0.9f, 0, 0.7f);
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +20,26 @@ public class ThrottleCalibration : MonoBehaviour
     // Update is called once per frame
     void Update()
     { 
-        Vector3 finalThrottlePos = new Vector3(rightHand.transform.position.x, throttleY, rightHand.transform.position.z);
+        Vector3 finalThrottlePos = new Vector3(rightHand.transform.localPosition.x, throttleY, rightHand.transform.localPosition.z);
         if (Input.GetKeyDown(KeyCode.T))
         {
-            GameObject newThrottle = Instantiate(throttle, finalThrottlePos + offset, Quaternion.Euler(0, rightHand.transform.eulerAngles.y, 0));
-            //newThrottle.transform.SetParent(heroModelsParent.transform);
+            
+
+            Vector3 handPos = rightHand.transform.position;
+
+           // GameObject newThrottle = Instantiate(throttle, shipGO.transform);
+
+            newThrottle.transform.localPosition = finalThrottlePos;
+            newThrottle.transform.localRotation = Quaternion.Euler(0, rightHand.transform.eulerAngles.y, 0);
+
+            Vector3 offset = newThrottle.transform.position - rightHand.transform.position;
+
+            newThrottle.transform.localPosition += new Vector3(offset.x, 0, offset.z + zOffset);
+
+            //if (throttleIsSpawned)
+            //{
+            //    Destroy(newThrottle);
+            //}
         }
     }
 }
--- NavyProjectU/Assets/Tec/Scripts/VR/VRCalibration.cs
+++ NavyProjectU/Assets/Tec/Scripts/VR/VRCalibration.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.InputSystem.XR;
 using UnityEngine.XR;
 
 public class VRCalibration : MonoBehaviour
 {
-    [SerializeField] private GameObject xrOrigin;
+    [SerializeField] private GameObject xrOriginGO;
     public GameObject leftHand, rightHand; 
     public GameObject leftWheelGripPoint, rightWheelGripPoint;
     public GameObject userOriginpos;
     public GameObject xrCamera;
     private Vector3 scaleOffset = new Vector3(0.05f, 0.1f, 0.0066666f);
+    [SerializeField] private XROrigin xrOrigin;
 
     public float cameraOffsetY = 1.1176f;
 
@@ -66,21 +68,31 @@ public class VRCalibration : MonoBehaviour
         Quaternion yAxisRotationOffset = Quaternion.Euler(0, offsetEulerAngles.y, 0); // Only use Y-axis
         Quaternion scaleRotationOffset = Quaternion.Euler(scaleOffset.x, scaleOffset.y, scaleOffset.z);
         
-        float distance = Vector3.Distance(realGripMidPoint, xrCamera.transform.position);
+        float distance = Vector3.Distance(realGripMidPoint, xrOrigin.CameraInOriginSpacePos) * 2;
         xrCamera.transform.rotation = yAxisRotationOffset * xrCamera.transform.rotation;
 
 
         // Set the position of the xr origins camera offset to the wheel + the offset of the hands and headset
-        xrOrigin.transform.position = new Vector3((virtualGripMidPoint.x + 0.04f), xrCamera.transform.position.y, (virtualGripMidPoint.z - distance));
-        xrCamera.transform.position = new Vector3((virtualGripMidPoint.x + 0.04f), xrCamera.transform.position.y, (virtualGripMidPoint.z - distance));
-        
 
+        Vector3 cameraOffset = xrOrigin.CameraInOriginSpacePos;
+        Vector3 localOffset = new Vector3(0.04f, 0, -distance);
+
+
+        // Get current camera position in world space
+        Vector3 cameraWorldPos = xrCamera.transform.position;
+
+        // Calculate how much we need to move the XR Origin to align camera and grip midpoint
+        Vector3 positionOffset = virtualGripMidPoint - cameraWorldPos;
+
+        // Apply the offset to XR Origin's position
+        xrOrigin.transform.position += positionOffset - new Vector3(0.1f, 0, 0.5f); // Slight manual offset
+        xrOriginGO.transform.position += positionOffset - new Vector3(0.1f, 0, 0.5f); 
         // Kind of works, needs some more adjustments but definetely some progress
 
 
     }
 
-    
+
     private IEnumerator CalibrationCountdown()
     {
         yield return new WaitForSeconds(3f);
