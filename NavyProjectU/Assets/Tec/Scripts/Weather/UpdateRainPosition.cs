cdf10c04ad14bd9885ea0af8843a68395fa9c899
up2150934 <bwlego@outlook.com>
2025-03-07 14:15:00 +0000

    Rain Improvements

--- NavyProjectU/Assets/Tec/Scripts/Weather/UpdateRainPosition.cs
+++ NavyProjectU/Assets/Tec/Scripts/Weather/UpdateRainPosition.cs
@@ -5,39 +5,19 @@ using UnityEngine;
 public class UpdateRainPosition : MonoBehaviour
 {
     [SerializeField] private GameObject rain;
-
+    [SerializeField] private Transform rainPosition;
     public bool rainIsOn = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        rain.transform.position = transform.position;
-        if (Input.GetMouseButtonDown(0))
-        {
-            ToggleRain(rainIsOn);
-        }
+        rainPosition.position = transform.position;
     }
-
-    public void ToggleRain(bool rainIsOn)
-    {
-        if (rainIsOn)
-        {
-            // code for on
-            rain.SetActive(true);
-            rainIsOn = !rainIsOn;
-        }
-        else
-        {
-            // code for off
-            rain.SetActive(false);
-            rainIsOn = !rainIsOn;
-            
-        }
-    }
-
+    public void ActivateRain() { rain.SetActive(true); }
+    public void DeactivateRain() { rain.SetActive(false); }
 }
