57929e10a1c39369a524f758a3721cff24818d56
tjaec <87785559+tjaec@users.noreply.github.com>
2025-03-25 14:20:17 +0000

    Updated Throttle and Wheel

--- NavyProjectU/Assets/Tec/Scripts/Physics/ShipMovement.cs
+++ NavyProjectU/Assets/Tec/Scripts/Physics/ShipMovement.cs
@@ -290,15 +290,15 @@ public class ShipMovement : MonoBehaviour
 
     public void SetThrottleModelRotation()
     {
-        throttleL.transform.eulerAngles = new Vector3(throttleL.transform.eulerAngles.x + accelPortInput * 90.0f, throttleL.transform.eulerAngles.y, throttleL.transform.eulerAngles.z);
-        throttleR.transform.eulerAngles = new Vector3(throttleR.transform.eulerAngles.x + accelStarboardInput * 90.0f, throttleR.transform.eulerAngles.y, throttleR.transform.eulerAngles.z);
+        throttleL.transform.localEulerAngles = new Vector3(accelPortInput * -90.0f, 0, 0);
+        throttleR.transform.localEulerAngles = new Vector3(accelStarboardInput * -90.0f, 0, 0);
     }
 
     public void SetSteeringWheelModelRotation()
     {
         if (steeringWheel != null)
         {
-            steeringWheel.transform.eulerAngles = new Vector3(0, 0, steeringInput);
+            steeringWheel.transform.localEulerAngles = new Vector3(0, 0, -steeringInput);
         }
         else Debug.LogError("Connect the steering wheel asset in the inspector");
     }
